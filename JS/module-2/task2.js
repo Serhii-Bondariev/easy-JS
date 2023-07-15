9ed02343924a0a7354ab13d7be99d2b61063967f
iVladyuser <korol.v09@gmail.com>
2023-07-15 11:41:46 +0200

    first_second_works

---
+++ JS/module-2/task2.js
@@ -0,0 +1,21 @@
+// У нас є об'єкт, у якому зберігаються зарплати
+//    нашої команди
+//    Напишіть код для підсумовування всіх зарплат і
+//    збережіть його результат у змінній sum.
+//    Якщо об'єкт salaries порожній, то результат має бути 0
+
+const salaries = {
+  Mango: 100,
+  Poly: 160,
+  Ajax: 1470,
+};
+
+function calc(salaries) {
+    let sum = 0;
+    for (const salary of Object.values(salaries)) {
+        sum += salary;
+    }
+    return sum;
+}
+
+console.log(calc(salaries));
